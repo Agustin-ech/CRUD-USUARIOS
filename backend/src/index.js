7ef4f95516fbe087457c129b176578a389d30f09
Agustin-ech <iaechavarriaa24@ul.edu.co>
2026-05-05 19:34:29 -0500

    init db

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,7 @@ import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
 import { PORT } from "./config.js";
 import { fileURLToPath } from "url";
+import path from "path";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
