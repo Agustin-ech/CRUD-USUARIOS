5f6ecda6a1960c1160d85f1b01de4be0fff64605
Agustin-ech <iaechavarriaa24@ul.edu.co>
2026-05-05 19:24:03 -0500

    init db

--- backend/src/db.js
+++ backend/src/db.js
@@ -7,10 +7,32 @@ import {
   DB_USER,
 } from "./config.js";
 
-export const pool = new pg.Pool({
+const { Pool } = pg;
+
+export const pool = new Pool({
   user: DB_USER,
   host: DB_HOST,
   password: DB_PASSWORD,
   database: DB_DATABASE,
   port: DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+// inicialización sin romper
+(async () => {
+  try {
+    await pool.query(`
+      CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(40),
+        email TEXT NOT NULL UNIQUE,
+        created_at TIMESTAMP NOT NULL DEFAULT NOW()
+      );
+    `);
+    console.log("Tabla lista ✅");
+  } catch (err) {
+    console.error("Error DB:", err);
+  }
+})();
