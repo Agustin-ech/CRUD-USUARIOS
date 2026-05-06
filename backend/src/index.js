8a4ee68415ba1e32a347f3e6dba8505de475f202
Agustin-ech <iaechavarriaa24@ul.edu.co>
2026-05-05 19:19:14 -0500

    init db

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,10 @@ import cors from "cors";
 import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
 import { PORT } from "./config.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -12,6 +16,11 @@ app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "../../frontend")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/index.html"));
+});
 
 app.use(usersRoutes);
 
